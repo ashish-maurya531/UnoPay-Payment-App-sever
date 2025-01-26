a61f3348874b80237959b260f668291c7d79e60d
Ashish Maurya <ashishanshumaurya1572002@gmail.com>
2025-01-26 09:28:48 +0530

    refactor: clean up getMembershipTransactionsForToday function and improve logging

--- backend/utills/companyTurnover.js
+++ backend/utills/companyTurnover.js
@@ -1,21 +1,19 @@
-const express = require('express');
 const { pool } = require('../config/database');
-const moment = require('moment'); // For handling date and time
+const moment = require('moment'); 
 
-// Function to get Membership transactions with success status for the current day
+
 const getMembershipTransactionsForToday = async (req, res) => {
     try {
-        // Get today's date in UTC format (YYYY-MM-DD) without the time part
-        const today = moment.utc().format('YYYY-MM-DD'); // Ensure it is in string format
+       
+        const today = moment.utc().format('YYYY-MM-DD'); 
 
-        // Debugging: Log today's date
+       
         console.log('Today\'s date (UTC):', today); 
 
         if (!today) {
             throw new Error('Invalid date');
         }
 
-        // SQL query to get all transactions where type is 'Membership', status is 'success', and the date is today
         const query = `
             SELECT * 
             FROM universal_transaction_table 
@@ -24,16 +22,14 @@ const getMembershipTransactionsForToday = async (req, res) => {
               AND DATE(created_at) = ?; 
         `;
 
-        // Execute the query with the current UTC date
         const [rows] = await pool.query(query, [today]);
 
-        // Send the response with the result
+     
         console.log({
             message: "Transactions fetched successfully",
             data: rows,
         });
     } catch (err) {
-        // Handle error
         console.error('Error fetching transactions:', err);
         console.log({
             message: "Error fetching transactions",
