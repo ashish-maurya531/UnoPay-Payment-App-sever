68a372305d9b03f0ca4d283ffdae76a131b27dc2
Ashish Maurya <ashishanshumaurya1572002@gmail.com>
2025-02-25 11:15:52 +0530

    file size

--- backend/routes/userBankKyc.js
+++ backend/routes/userBankKyc.js
@@ -27,7 +27,7 @@ subDirs.forEach((subDir) => {
 const storage = multer.memoryStorage();
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
+  limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 5MB
   fileFilter: (req, file, cb) => {
     const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
     if (allowedTypes.includes(file.mimetype)) {
